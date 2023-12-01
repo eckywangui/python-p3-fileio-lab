3995fed71ad1ca2ffa98b457888fc4b6a6166096
Eckra Wangui <+>
2023-12-01 17:41:39 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    file_path = f"{file_name}.txt"
+    with open (file_path, "w") as file:
+        file.write (file_content)
 
-def append_file(file_name, append_content):
+def append_file(file_name, append_content):  
+    with open(f'{file_name}.txt', 'a') as f:
+        f.write(append_content)
     pass
 
 def read_file(file_name):
+    with open (f"{file_name}.txt") as f:
+     return f.read ()
     pass
